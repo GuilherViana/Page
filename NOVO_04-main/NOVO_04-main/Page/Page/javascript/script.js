9631c1a36f728909075c3405a6e3b9dc1b590412
Guilherme Viana Santana <115712050+GuilherViana@users.noreply.github.com>
2024-09-09 07:46:43 -0300

    Add files via upload

---
+++ NOVO_04-main/NOVO_04-main/Page/Page/javascript/script.js
@@ -0,0 +1,9 @@
+function showSignUp() {
+    document.getElementById('login-box').style.display = 'none';
+    document.getElementById('sign-up-box').style.display = 'block';
+}
+
+function showLogin() {
+    document.getElementById('sign-up-box').style.display = 'none';
+    document.getElementById('login-box').style.display = 'block';
+}
